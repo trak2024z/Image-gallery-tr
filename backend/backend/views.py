391105cb0f7d2f612e8d2818fc0312d11389cca9
Remigiusz Sieczkowski <remik285@gmail.com>
2024-04-14 16:15:55 +0200

    Added GET endpoints with placeholder data sending

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -7,3 +7,59 @@ def send_some_data(request):
     return JsonResponse({
         'data': 'Hello from django backend'
     })
+
+@api_view(['GET'])
+def get_all_galeries(request):
+    return JsonResponse(
+        {
+            'galeries':'All galleries'
+        })
+    
+@api_view(['GET'])
+def get_gallery(request, gallery_id):
+    return JsonResponse(
+        {
+            'galery':'Gallery with id: ' + str(gallery_id)
+        })
+
+@api_view(['GET'])
+def get_all_pictures(request, gallery_id):
+    return JsonResponse(
+        {
+            'pictures':'All pictures'
+        })
+
+@api_view(['GET'])
+def get_picture(request, picture_id):
+    return JsonResponse(
+        {
+            'pictures':'Picture with id: ' + str(picture_id)
+        })
+
+@api_view(['GET'])
+def get_all_comments(request, picture_id):
+    return JsonResponse(
+        {
+            'comments':'All comments at picture with id: ' + str(picture_id)
+        })
+
+@api_view(['GET'])
+def get_rating(request, picture_id):
+    return JsonResponse(
+        {
+            'rating':'Rating at picture with id: ' + str(picture_id)
+        })
+
+@api_view(['GET'])
+def get_user_info(request, user_name):
+    return JsonResponse(
+        {
+            'user':'Info of user with name: ' + user_name
+        })
+    
+@api_view(['GET'])
+def get_user_permissions_to_gallery(request, gallery_id):
+    return JsonResponse(
+        {
+            'users':'Users with permission to gallery'
+        })
