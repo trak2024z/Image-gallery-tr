c557cf21c84c0a94a250e6b382fce9fcff50063a
weddom <dominikwedrychowicz@gmail.com>
2024-05-28 21:24:33 +0200

    Revert "main-page-view-not-working"
    This reverts commit e2208937ff177f3becc42759a010e6e335bcdd85.

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,41 +1,17 @@
-import React, { useState } from "react";
-import ProgressBar from './components/ProgressBar';
-
+import React from "react";
 const Home = () => {
+
     const token = localStorage.getItem("token");
     const userJson = localStorage.getItem("user");
     const user = userJson ? JSON.parse(userJson) : null;
-
-    const [file, setFile] = useState(null);
-    const [error, setError] = useState(null);
-    const types = ['image/png', 'image/jpeg'];
-
-    const handleChange = (e) => {
-    let selected = e.target.files[0];
-
-    if (selected && types.includes(selected.type)) {
-      setFile(selected);
-      setError('');
-    } else {
-      setFile(null);
-      setError('Please select png or jpg image file');
-    }
-  };
     return (
         <div>
+            <h1>Home</h1>
             {token && user ? (
-                <form>
-                <label>
-                  <input type="file" onChange={handleChange} />
-                  <span>+</span>
-                </label>
-                <div className="output">
-                  { error && <div className="error">{ error }</div>}
-                  { file && <div>{ file.name }</div> }
-                  { file && <ProgressBar file={file} setFile={setFile} /> }
+                <div>
+                    <h1>Welcome, {user.username}</h1>
+                    <a href="/logout">Logout</a>
                 </div>
-                <a href="/logout">Logout</a>
-              </form>
             ) : (
                 <div>
                     <p>Nie jesteś zalogowany.</p>
